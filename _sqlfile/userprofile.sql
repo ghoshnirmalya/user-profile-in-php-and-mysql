e87a5135b0fc60bf5d9e8c6180184c0cb2fdad8b
Nirmalya Ghosh <nirmalya.email@gmail.com>
2015-06-28 01:09:07 +0530

    Updated .sql file

--- _sqlfile/userprofile.sql
+++ _sqlfile/userprofile.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.0
+-- version 4.0.10.7
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 26, 2014 at 07:50 PM
--- Server version: 5.5.38
--- PHP Version: 5.3.28
+-- Generation Time: Jun 27, 2015 at 03:27 PM
+-- Server version: 5.5.44-MariaDB
+-- PHP Version: 5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `userprofile`
+-- Database: `infismas_aboutme`
 --
 
 -- --------------------------------------------------------
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-`user_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_email` varchar(255) NOT NULL,
   `user_password` varchar(255) NOT NULL,
   `user_firstname` text NOT NULL,
@@ -35,39 +35,28 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_avatar` varchar(255) NOT NULL,
   `user_shortbio` text,
   `user_username` varchar(255) NOT NULL,
+  `user_longbio` text,
+  `user_facebook` varchar(255) DEFAULT NULL,
+  `user_twitter` varchar(255) DEFAULT NULL,
+  `user_linkedin` varchar(255) DEFAULT NULL,
+  `user_stumbleupon` varchar(255) DEFAULT NULL,
+  `user_pinterest` varchar(255) DEFAULT NULL,
+  `user_googleplus` varchar(255) DEFAULT NULL,
+  `user_website` varchar(255) DEFAULT NULL,
   `user_dob` date DEFAULT NULL,
   `user_profession` text,
   `user_gender` varchar(255) DEFAULT NULL,
+  `user_maritialstatus` varchar(255) DEFAULT NULL,
+  `user_address` text,
+  `user_backgroundpicture` varchar(255) NOT NULL,
   `user_joindate` date NOT NULL,
-  `user_country` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+  `user_country` varchar(255) DEFAULT NULL,
+  `user_skype` varchar(255) DEFAULT NULL,
+  `user_github` varchar(255) DEFAULT NULL,
+  `user_youtube` varchar(255) DEFAULT NULL,
+  `user_vimeo` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_email` (`user_email`),
+  UNIQUE KEY `user_username` (`user_username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1480 ;
 
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`user_id`, `user_email`, `user_password`, `user_firstname`, `user_lastname`, `user_avatar`, `user_shortbio`, `user_username`, `user_dob`, `user_profession`, `user_gender`, `user_joindate`, `user_country`) VALUES
-(18, 'nilabrata@gupta.com', 'nilabrata', 'Nilabrata', 'Gupta', 'default.jpg', '', 'nilabrata', '2014-10-08', NULL, NULL, '2014-10-27', NULL);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`user_id`), ADD UNIQUE KEY `user_email` (`user_email`), ADD UNIQUE KEY `user_username` (`user_username`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
