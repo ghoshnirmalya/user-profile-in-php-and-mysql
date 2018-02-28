134f7aba229c06f3bff199ac5881db49e89b4e77
macperigren <macperigren@gmail.com>
2018-03-01 01:21:52 +0800

    Update check-username-availability.php

--- components/check-username-availability.php
+++ components/check-username-availability.php
@@ -6,7 +6,7 @@
         if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
             die();
         }
-        $username = $_POST['user_username'];
+        $username = isset($_SESSION['user_username']);
         $username = mysqli_real_escape_string($database,$username);
         $sql_check = mysqli_query($database,"SELECT user_username FROM user WHERE user_username='$username'");
         if(mysqli_num_rows($sql_check)){
